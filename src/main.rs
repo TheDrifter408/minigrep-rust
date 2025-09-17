d6104211ef67672ec18cd5eb27d046508e330b2e
Nurullah <nurullahan408@gmail.com>
2025-09-17 14:41:02 +0600

    Adding the minimal necessary code in main 1. Added a Config struct to consolidate logic 2. Added a run function with error handling as per the guidelines

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,48 @@
 // We need to use the std::env::args function from the Standard Library;
 use std::env;
+// fs library to use functions for reading and writing files
+use std::fs;
 
+use std::process;
+
+use std::error::Error;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // The first element will be the file path and my mini-grep will need
-    // to
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    print!("Query: {query}");
-    print!("File path: {file_path}");
+    println!("Searching for {}", config.search_query);
+    println!("In file: {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        println!("Application Error: {e}");
+        process::exit(1);
+    };
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+
+    let file_contents = fs::read_to_string(config.file_path)?;
+    println!("Contents:\n {file_contents}");
+
+    Ok(())
+}
+
+struct Config {
+    search_query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments given");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { search_query: query, file_path })
+    }
 }
